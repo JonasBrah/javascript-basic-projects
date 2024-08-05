7021106bdac91c13cad77609c195833dc7cfa56f
JonasBrah <jonasbrahmas@gmail.com>
2024-08-05 15:32:11 -0500

    coomit 5 sidebar ofi

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,20 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle= document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+const socialicons=document.querySelector(".social-icons");
+navToggle.addEventListener("click", function(){
+ // console.log(links.classList);
+   
+  //console.log(socialicons.classList);
+     /*if (links.classList.contains("show-links")){
+     links.classList.remove("show-links");
+    }else{
+     links.classList.add("show-links")};*/
+    links.classList.toggle("show-links");//Activa y desactiva la aparicion de la clase showlinks
+})
+
+
